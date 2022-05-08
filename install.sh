d0840aa5ef80a8a8f900c09604e8cc33869b0fd3
phamngsinh <phamngsinh39@gmail.com>
2022-05-08 10:11:45 +0700

    install

---
+++ install.sh
@@ -0,0 +1,19 @@
+sudo snap install skype ;
+sudo apt install filezilla ;
+sudo snap install phpstorm --classic ;
+sudo snap install pycharm-community --classic ;
+sudo add-apt-repository ppa:ondrej/php ;
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash ;
+sudo apt install vim ;
+sudo apt install git ;
+sudo snap install dbeaver-ce ;
+#connect online account
+#install franz FileZilla
+
+#franz
+#anydesk
+#FileZilla
+#smartgit
+#docker
+#tor-browser
+#google-chrome
